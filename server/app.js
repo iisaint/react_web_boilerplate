f996cdbba0c8638312f38738fb1b30da75627e9c
Yao-Hsin Chen <iisaintz@gmail.com>
2019-04-21 14:22:35 +0800

    setup express

---
+++ server/app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+const port = process.env.PORT || 8081;
+let server = app.listen(port, function() {
+  let host = server.address().address;
+
+  console.info('API at http://%s:%s', host, port);
+});
+
+module.exports = app;
